41c5a6192587a9c01cbabfba0c7ccec5b5aeca3b
xyapple <ydeascentis@gmail.com>
2017-07-06 12:07:43 -0400

    Add the video.props into the style

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -3,15 +3,16 @@ import React from 'react';
 const VideoListItem = ({video}) => {
     //console.log(video);
     //const video = props.video;
+    const imageURL = video.snippet.thumbnails.default.url;
     return (
         <li className="list-group-item">
             <div className="video-list media">
                 <div className='media-left'>
-                    <img className='image-object' />
+                    <img className='image-object' src={imageURL} />
                 </div>
-                
+
                 <div className='media-body'>
-                    <div className='media-heading'></div>
+                    <div className='media-heading'>{video.snippet.title}</div>
                 </div>
 
             </div>
