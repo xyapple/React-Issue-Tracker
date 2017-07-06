70a60d2e8a540d052b6e312229dcc59c57b55684
xyapple <ydeascentis@gmail.com>
2017-07-06 11:31:24 -0400

    Work on Ajax call for API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//load the libary
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/searchBar';
+
 //Google API Key
 const API_KEY = 'AIzaSyB8Sokjv4f6Bfvuo2UTTk33JuV9XcV_5LE';
 
-const App =()=>{
+YTSearch({key: API_KEY, term:'kayak'}, function(data){
+    console.log(data);
+})
+
+const App = () => {
     return (
         <div>
-
-            <SearchBar />
+            <SearchBar/>
         </div>
 
     )
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <App/>, document.querySelector('.container'));
